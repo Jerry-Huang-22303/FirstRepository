603a4088a6d22003a8cfc2bddbbf16541eb4a3f0
Jiangrong <jhuang303@yahoo.ca>
2020-03-24 22:34:03 -0400

    revert

--- FirstRepository/Program.cs
+++ FirstRepository/Program.cs
@@ -11,8 +11,6 @@ namespace FirstRepository
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.WriteLine("Hi");
-            Console.WriteLine("Welcome");
         }
     }
 }
