163cd9cefb67aa3224e762f90ace734f0e52a586
Jiangrong <jhuang303@yahoo.ca>
2020-03-24 22:07:03 -0400

    Add project files.

---
+++ FirstRepository/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstRepository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
